53073a294b3860cc6b024c337844e5c34a377b8c
Martin Meincke <meincke91@hotmail.com>
2018-03-21 09:19:39 +0100

    X-code project setup

---
+++ legendary-waffle/legendary-waffle/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  legendary-waffle
+//
+//  Created by Martin Meincke on 21/03/2018.
+//  Copyright © 2018 Martin Meincke. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
