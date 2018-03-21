ec8ecf8b19f157f49c1b0c2a41a1399c2ed75088
esbenlk <esben@loevendal.dk>
2018-03-21 19:30:04 +0100

    Integrated Apple WatchKit

---
+++ legendary-waffle/Stress reflector Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  Stress reflector Extension
+//
+//  Created by Esben Kruse on 21/03/2018.
+//  Copyright © 2018 Martin Meincke. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+        super.awake(withContext: context)
+        
+        // Configure interface objects here.
+    }
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
