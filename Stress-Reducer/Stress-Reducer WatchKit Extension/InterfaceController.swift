524e8a4e0f12a3af7c52bfae2df440d67d2423a1
Martin Meincke <meincke91@hotmail.com>
2018-04-04 13:37:23 +0200

    Created a new shared iPhone and watch app project

---
+++ Stress-Reducer/Stress-Reducer WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,34 @@
+//
+//  InterfaceController.swift
+//  Stress-Reducer WatchKit Extension
+//
+//  Created by Martin Meincke on 04/04/2018.
+//  Copyright © 2018 Martin Meincke. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+        super.awake(withContext: context)
+        
+        // Configure interface objects here.
+    }
+    @IBAction func next_press() {
+        print("did push button")
+    }
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
